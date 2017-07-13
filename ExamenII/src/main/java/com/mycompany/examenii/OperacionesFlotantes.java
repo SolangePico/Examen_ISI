77890fb812f05681cc2efb093658be644223dd5b
dasurezm <dasurezm@espe.edu.ec>
2017-07-12 20:42:31 -0500

    Creacion de los metodos multiplicar y division en la clase operacionesFlotante

--- ExamenII/src/main/java/com/mycompany/examenii/OperacionesFlotantes.java
+++ ExamenII/src/main/java/com/mycompany/examenii/OperacionesFlotantes.java
@@ -10,5 +10,26 @@ package com.mycompany.examenii;
  * @author solan
  */
 public class OperacionesFlotantes {
-    
+    /**
+     * metodo que devuleve la multiplicacion entre flotantes.
+     * @param n1
+     * @param n2
+     * @return 
+     */
+    public float multiplicacion(float n1, float n2){
+        return n1*n2;
+    }
+    /**
+     * Metodo que devueleve la division entre flotantes.
+     * @param n1
+     * @param n2
+     * @return 
+     */
+    public float division(float n1, float n2){
+        if (n2 == 0) {
+            return -1;
+        }else{
+            return n1/n2;
+        }
+    }
 }
