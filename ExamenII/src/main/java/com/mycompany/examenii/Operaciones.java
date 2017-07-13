5775c99caa68db6ee59db694c7fb0a3e05a7f92f
Alejandro Torres <Alejandro Torres@10.9.4.71>
2017-07-12 20:19:56 -0500

    Clase suma y resta.

--- ExamenII/src/main/java/com/mycompany/examenii/Operaciones.java
+++ ExamenII/src/main/java/com/mycompany/examenii/Operaciones.java
@@ -1,7 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Clase que contiene las operaciones matemáticas entre dos números enteros.
+ * @author Alejandro Torres, Solange Pico, David Suaréz.
+ * @version 1.0
+ * 
  */
 package com.mycompany.examenii;
 
@@ -10,5 +11,21 @@ package com.mycompany.examenii;
  * @author dovac
  */
 public class Operaciones {
+    
+/**
+* Método que realiza la suma entre dos números enteros. 
+* @return the a + b
+*/
+public static int Suma(int a, int b) {
+    return a + b;
+}
+
+/**
+* Método que realiza la resta entre dos números enteros. 
+* @return the a - b
+*/
+public static int Resta(int a, int b) {
+    return a - b;
+}
     
 }
