c41ce86a02caa55a652809f0d219106cdcdf7f6f
Solange Pico <svpico@espe.edu.ec>
2017-07-12 20:37:09 -0500

    Creacion de la clase operaciones flotantes.

--- ExamenII/src/main/java/com/mycompany/examenii/Started.java
+++ ExamenII/src/main/java/com/mycompany/examenii/Started.java
@@ -5,10 +5,48 @@
  */
 package com.mycompany.examenii;
 
+import java.util.Scanner;
+
 /**
  *
  * @author solan
  */
 public class Started {
+
+        //Campos de la clase
+    Scanner sc = new Scanner(System.in);
+    private int op;
+    private int p;
     
-}
+    //public void menu{
+    /*do {
+            do {
+                System.out.println();
+                System.out.println("Bienvenido");
+                System.out.println("1. Suma de números");
+                System.out.println("2. Resta de número");
+                System.out.println("3. Multiplicacion");
+                System.out.println("4. Division");
+                System.out.println("5. Salir");
+                System.out.println();
+                System.out.println("Eliga una opción: ");
+                op = sc.nextInt();
+                if (op < 0 || op > 5) {
+                    System.out.println("!Opción Incorrecta, introduzca una válida");
+                }
+            } while (op < 0 || op > 7);
+            switch (op) {
+                case 1:
+                    fac.inicio();
+                    do{
+                    System.out.println("Desea calcular otro numero: (1.S/2.N)");
+                    p = sc.nextInt();
+                    if (p == 1) {
+                        fac.inicio();
+                    } else {
+                        System.out.println("Saliendo de factorial");
+                        break;
+                    }
+                    }while(p == 1);
+                    break;*/
+            }
